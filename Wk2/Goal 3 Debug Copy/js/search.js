2cafc742738a7567b58737b78d4e3954cb738dc4
Joshua Ceres <jmc.usmc81@gmail.com>
2014-08-15 18:45:17 -0400

    Updating and moving around curly braces trying to make code work

--- Wk2/Goal 3 Debug Copy/js/search.js
+++ Wk2/Goal 3 Debug Copy/js/search.js
@@ -8,56 +8,72 @@
 
 (function() {
     var resultsDIV = document.getElementById("results"),    //You can just use a comma then Semicolon at the end of statement?
-    searchInput = document.forms[0].search,
-    currentSearch = ''                                      //Code is nice and neat
+        searchInput = document.forms[0].search,                 //If I opus semicolon after line 11 the currentSearch turns purple.
+        currentSearch = ''                                      //Code is nice and neat
         ;
 
-   var validate = function(query) {                         // Changed to a single equal sign and spelled validate the right way
+    var validate = function (query) {                         // Changed to a single equal sign and spelled validate the right way
 
-       while (query.charAt(0) === " ") {
-           query = query.substring(1, query.length);
-       };
-
-
-       while (query.charAt(query.length - 1) === "") {
-           query = query.substring(0, query.length - 1);
-       };
+        while (query.charAt(0) === " ") {
+            query = query.substring(1, query.length);
+        }
 
 
-       if (query.length < 3) {
-           alert("Your search query is too small, try again.");      // Added quotation mark to the end of the statement. It was missing.
 
-           searchInput.focus();
-           return;
-       };
+        while (query.charAt(query.length - 1) === "") {
+            query = query.substring(0, query.length - 1);
+        }
 
-       search(query);
-   };
 
-        var search = function(query){                       // Added a curly bracket
-            var queryArray = query.split(" ");             // Changed from query.join to query.split
-            var results = [];
-            for(var i=0, j=db.length; i<j; i++){
-                var dbTitleEnd = db[i].indexOf('|');
-                var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
-            };
 
-            for(var ii=0, jj=queryArray.length; ii<jj; ii++){
-                var qitem = queryArray[ii].tolowercase();
-                var compare = dbitem.indexOf(qitem);
-                if(compare !== -1){
-                    results.push(db[i]);
-                };
-            };
+        if (query.length < 3) {
+            alert("Your search query is too small, try again.");      // Added quotation mark to the end of the statement. It was missing.
 
-        };
+            searchInput.focus();
+            return;
+        }
+
+
+        search(query);
+    };
+
+    var search = function (query) {                                    // Added a curly bracket
+        var queryArray = query.split(" ");                          // Changed from query.join to query.split
+        var results = [];
+        for (var i = 0, j = db.length; i < j; i++) {
+            var dbTitleEnd = db[i].indexOf('|');
+            var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+        }
+
+
+        for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {
+            var qitem = queryArray[ii].tolowercase();
+            var compare = dbitem.indexOf(qitem);
+            if (compare !== -1) {
+                results.push(db[i]);
+            }
+
+        }
+
+
+
 
     results.sort();
-        if(results.length = 0){
-            noMatch();
-        }else{
-            showMatches(results);
-        };
+
+    if (results.length = 0) {
+        noMatch();
+    } else {
+        showMatches(results);
+    }
+
+  };
+    var noMatch = function(){
+        var html = '' +
+                '<p>No Results found.</p>'+
+                '<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
+            ;
+        resultsDIV.innerHTML = html;
+    };
 
         var showMatches = function(results) {
 
@@ -65,7 +81,7 @@
             var html = '<p>Results</p>',
                 title,
                 url
-            ;
+                ;
 
             for (var i = 0, j = results.length; i < j; i++) {
                 titleEnd = results[i].indexOf('|');
@@ -74,15 +90,16 @@
                 url = results[i].substring(results[i].indexOf('|') + 1, results[i].length);
 
                 html += '<p><a href=' + url + '>' + title + '</a></p>';
-            };
+            }
+
 
             resultsDIV.innerHTML = html;
-
+        };
             document.forms[0].onsubmit = function () {
                 var query = searchInput.value;
                 validate(query);                                        //validate spelled correctly
                 return false;
             };
-        };
+
 
 })();

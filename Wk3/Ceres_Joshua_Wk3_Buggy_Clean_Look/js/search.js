9b252049b694b7ae42520188cbb3e61e0c9bd4d5
Joshua Ceres <jmc.usmc81@gmail.com>
2014-08-21 12:36:39 -0400

    Copied week 2 assignments and pased inside week 3 with new directory names

---
+++ Wk3/Ceres_Joshua_Wk3_Buggy_Clean_Look/js/search.js
@@ -0,0 +1,114 @@
+/**
+ * Created by Josh on 8/14/14.
+ */
+// Debugging search.js
+// Week 2
+// Buggy assignment v2.2
+
+
+(function() {
+    var resultsDIV = document.getElementById("results"),    //You can just use a comma then Semicolon at the end of statement?
+        searchInput = document.forms[0].search,                 //If I opus semicolon after line 11 the currentSearch turns purple.
+        currentSearch = ''                                      //Code is nice and neat
+        ;
+
+    var validate = function (query) {                         // Changed to a single equal sign and spelled validate the right way
+
+        while (query.charAt(0) === " ") {
+            query = query.substring(1, query.length);
+        }                                                          //No semicolon needed
+
+
+
+        while (query.charAt(query.length - 1) === "") {
+            query = query.substring(0, query.length - 1);
+        }                                                           //No semicolon needed
+
+
+
+        if (query.length < 3) {                                        //ALERTS user if the search input is to small
+            alert("Your search query is too small, try again.");      // Added quotation mark to the end of the statement. It was missing.
+
+            searchInput.focus();
+            return;
+        }
+
+
+        search(query);
+    };                                                              //Put Curly bracket here because sear(query); was coming up undefined
+
+    var search = function (query) {                                 // Added a curly bracket
+        var queryArray = query.split(" ");                          // Changed from query.join to query.split
+        var results = [];
+        for (var i = 0, j = db.length; i < j; i++) {
+            var dbTitleEnd = db[i].indexOf('|');
+            var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+        }                                                      //No Semicolon needed
+
+
+        for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {
+            var qitem = queryArray[ii].tolowercase();
+            var compare = dbitem.indexOf(qitem);
+            if (compare !== -1) {
+                results.push(db[i]);
+            }                                                   //No semicolon needed
+
+        }                                                       //No semicolon needed and added curly bracket
+
+
+
+
+    results.sort();
+
+    if (results.length = 0) {
+        noMatch();
+    } else {
+        showMatches(results);
+    }
+
+  };                                                            //This curly bracket closes the var search function on line 40
+    var noMatch = function(){                                   //I initially forgot to copy this line of code over and had to go back
+        var html = '' +                                         //and scan the first document that had no edits to find what I was missing
+                '<p>No Results found.</p>'+
+                '<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
+            ;
+        resultsDIV.innerHTML = html;
+    };
+
+        var showMatches = function(results) {
+
+
+            var html = '<p>Results</p>',
+                title,
+                url
+                ;
+
+            for (var i = 0, j = results.length; i < j; i++) {
+                titleEnd = results[i].indexOf('|');
+                title = results[i].subString(0, titleEnd);
+
+                url = results[i].substring(results[i].indexOf('|') + 1, results[i].length);
+
+                html += '<p><a href=' + url + '>' + title + '</a></p>';
+            }                                                           //No semicolon needed
+
+
+            resultsDIV.innerHTML = html;
+        };
+            document.forms[0].onsubmit = function () {
+                var query = searchInput.value;
+                validate(query);                                        //validate spelled correctly
+                return false;
+            };                                                             //Added curly Bracket
+
+
+})();                                                                   //This line of code closes the beginning function on line 9
+
+/***                                    ----------Notes Wk2 Buggy v2------------
+
+I added notes section or this piece of work since I copied over everything from the other js file and removed all the comment.
+I left my own comments for what I did for debugging. This is a more neat and clean look and is not filled with to much text that can be a bit
+draining reading through it all. I had a bit of trouble placing curly brackets in the right spots to make the coding function correctly.
+I was getting some undefined and such for some of the codes because curly brackets were int he wrong spots.
+
+ ***/
---
+++ Wk3/Ceres_Joshua_Wk3_Buggy_v3.0/js/search.js
@@ -0,0 +1,157 @@
+//Joshua Ceres
+//Assignment: Debug Buggy Search.v2: PWA1
+//August 14-18 2014
+
+
+
+// Create privatized scope using a self-executing function
+(function() {
+
+    // Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
+
+    var resultsDIV = document.getElementById("results"),
+                                                                    //variables not defined and do not have a semicolon...syntax error?
+    searchInput = document.forms[0].search,                         // var?? variables not defined and do not have a semicolon...syntax error?\
+    currentSearch = ''                                             //---Week 2--- cleaned up lines of code and made it more organized.
+;
+
+    var validate = function(query) {                                     //---Week 2---Removed an equal sign and spelled validate proper way
+
+        // Trim whitespace from start and end of search query
+        while (query.charAt(0) === " ") {                               //---Week 2--- fixed code , it was placed outside the while loop
+            query = query.substring(1, query.length);                  //---Week 2--- added triple equal to query/charAt
+        }                                                               //---Week 2 added Bracket and removed semi colon
+
+
+        while (query.charAt(query.length - 1) === "") {
+            query = query.substring(0, query.length - 1);
+        }                                                               //---Week 2---Removed semicolon
+
+
+            // Finds search matches
+            // loop through each index of db array
+            // each db[i] is a single video item, each title ends with a pipe "|"
+            // save a lowercase variable of the video title
+
+            // array to store matched results from database.js
+            // (DO NOT FIX THE LINE DIRECTLY BELOW)
+
+            // Semicolon for what reason??
+
+            // Check search length, must have 3 characters
+
+            if (query.length < 3) {                                     //Search to make sure the User Input is less then 3
+                alert("Your search query is too small, try again.");   //---Week 2--- added quotation mark at the end of statement
+                searchInput.focus();
+                return;                                                //return what?
+            }
+
+
+
+            search(query);
+        };                                                            //--Week 2-- Closes var validate function
+
+        // Variable search defined somewhere??
+
+        var search = function (query) {                                     // --Week 2-- Inserted Curly Brackets
+
+            // split the user's search query string into an array
+            var queryArray = query.split(" ");                             // ---Week 2 --- Changed from query.join to query.split
+            var results = [];
+            for (var i = 0, j = db.length; i < j; i++) {
+                var dbTitleEnd = db[i].indexOf('|');                            //--Week 2--- Organized and cleaned up
+                var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+            }                                                                       //--Week 2--- Removed Semicolon
+                // loop through the user's search query words
+                // save a lowercase variable of the search keyword
+                // Check that matches were found, and run output functions
+                // The onsubmit event will be reviewed in upcoming Course Material.
+                // THE LINE DIRECTLY BELOW IS CORRECT
+                // return false is needed for most events - this will be reviewed in upcoming course material
+                // THE LINE DIRECTLY BELOW IS CORRECT
+
+                // Put matches into page as paragraphs with anchors
+
+                // Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
+                // is the keyword anywhere in the video title?
+                // If a match is found, push full db[i] into results array
+                for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {
+
+                    var qitem = queryArray[ii].tolowercase();
+                    var compare = dbitem.indexOf(qitem);                       // db.item? or meant to be dbItem?
+
+                    if (compare !== -1) {
+                        results.push(db[i]);
+                    }
+
+
+                }
+
+
+
+
+
+            results.sort();                                                     //--Week 2--Cleaned up
+
+            if (results.length = 0) {
+                noMatch();                                                      //Variable noMatch defined somewhere?
+            } else {
+                showMatches(results);                                           //Variable showMatches defined somewhere?
+            }
+
+
+        };                                                                      //---Week 2--- Closes var search function
+
+
+        var noMatch = function () {
+            var html = '' +
+                    '<p>No Results found.</p>' +
+                    '<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
+                ;
+            resultsDIV.innerHTML = html;
+        };
+
+        var showMatches = function (results) {
+
+            // THE NEXT 4 LINES ARE CORRECT.
+            var html = '<p>Results</p>',                                          //--Week 2--- Cleaned up
+                title,
+                url
+                ;
+
+
+            // loop through all the results search() function
+            for (var i = 0, j = results.length; i < j; i++) {
+
+                // title of video ends with pipe
+                // pull the title's string using index numbers
+                titleEnd = results[i].indexOf('|');                             //titleEnd Declared somewhere?
+                title = results[i].subString(0, titleEnd);                      //I do not think subString is written correctly. Runtime error
+
+                // pull the video url after the title
+                url = results[i].substring(results[i].indexOf('|') + 1, results[i].length);
+
+                // make the video link - THE NEXT LINE IS CORRECT.
+                html += '<p><a href=' + url + '>' + title + '</a></p>';
+            }
+
+
+            resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
+        };                                                                          //---Week 2---Closes var showMatches function
+
+            document.forms[0].onsubmit = function () {
+            var query = searchInput.value;
+
+            validate(query);                                          //--Week 2---Spelled validate correctly
+            return false;
+
+
+        };                                                             //---Week 2--- Added Curly brace
+
+                                                                   //---Week 2 ---Added curly brace and semicolon
+
+})();                                                               //---Week 2--- Closes entire function
+
+
+
+//I have added a second debug copy with less comments and a cleaner look to the coding.

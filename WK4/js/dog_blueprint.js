640e7e8c36839d35426cd8ad4b11f2dd68857ee9
Joshua Ceres <jmc.usmc81@gmail.com>
2014-08-28 10:40:30 -0400

    Following along with Fia

--- WK4/js/dog_blueprint.js
+++ WK4/js/dog_blueprint.js
@@ -4,3 +4,14 @@
 
 
 //Step 1 : Setup Constructor Function
+
+function Dog(n, c, b){
+    console.log("New Dg Added to Kennel!");
+    this.name = n; //dog's name
+    this.color = c;  //dog's color
+    this.breed = b;  //dog's breed
+
+}
+
+
+//Step 2

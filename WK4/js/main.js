7176a3e5313710aae56bdc166164cca29b955cbf
Joshua Ceres <jmc.usmc81@gmail.com>
2014-08-28 10:47:44 -0400

    Assigned 1st instnce to main function

--- WK4/js/main.js
+++ WK4/js/main.js
@@ -2,3 +2,14 @@
 /**
  * Created by Josh on 8/28/14.
  */
+
+
+function(){     //Self executing function
+
+    //Create !st Instance of the dog object and assign it myDog variable and running Constructor Function
+    var myDog1 = new Dog();
+    myDog1.name = "Max";
+    myDog1.color = "Brown";
+    myDog1.breed = "German Shepard";
+    myDog1.bark("Woof! Woof! Wooff!!")
+}
